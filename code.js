ec812952c6ee00096c3f68f5feb1dfade6b1c2b9
tharindadamruwan <tharindadamruwan@gmail.com>
2022-05-31 09:48:20 +0530

    first commit

---
+++ code.js
@@ -0,0 +1,141 @@
+let API = "https://www.themealdb.com/api/json/v1/1/";
+
+let app = document.querySelector(".app")
+
+let screen = {
+  main: app.querySelector(".main-screen"),
+  recipe: app.querySelector(".recipe-screen")
+};
+
+(async function() {
+
+  let res = await fetch(API + "list.php?c=list");
+  let data = await res.json();
+  let categories = data.meals;
+
+  for (let i = 1; i < categories.length; i++) {
+
+    let div = document.createElement("div");
+
+    div.innerText = categories[i].strCategory;
+    div.addEventListener("click", function() {
+
+      screen.main.querySelector(".categories .active").classList.remove("active");
+      div.classList.add("active");
+
+      getRecipesOfCatagoty(categories[i].strCategory);
+
+    });
+
+    if (i == 1) {
+
+      div.classList.add("active");
+      getRecipesOfCatagoty(categories[i].strCategory);
+
+    }
+
+    screen.main.querySelector(".categories").appendChild(div);
+
+  }
+
+})();
+
+async function getRecipesOfCatagoty(catagory) {
+  try {
+
+    screen.main.querySelector(".recipe-list").innerHTML = "";
+
+    let res = await fetch(API + "filter.php?c=" +catagory);
+    let data = await res.json();
+    let recipes = data.meals;
+
+    for (let i = 0; i < recipes.length; i++) {
+
+      let div = document.createElement("div");
+
+      div.classList.add("item");
+
+      div.addEventListener("click", function() {
+
+        showFullResipe(recipes[i].idMeal);
+
+      });
+
+      div.innerHTML = `
+      <div class="thumbnail">
+
+      <img src="${recipes[i].strMealThumb}"/>
+
+      </div>
+
+      <div class="details">
+
+      <h2>${recipes[i].strMeal}</h2>
+
+      </div>
+
+      `;
+
+      screen.main.querySelector(".recipe-list").appendChild(div);
+
+    }
+
+  } catch (msg) {}
+
+}
+
+async function showFullResipe(recipeId) {
+
+  screen.main.classList.add("hidden");
+  screen.recipe.classList.remove("hidden");
+  screen.recipe.querySelector(".back-btn").addEventListener("click", function() {
+
+    screen.recipe.classList.add("hidden");
+    screen.main.classList.remove("hidden");
+    screen.recipe.querySelector(".thumbnail img").src = "";
+    screen.recipe.querySelector(".details h2").innerText = "";
+    screen.recipe.querySelector(".details ul").innerHTML = "";
+    screen.recipe.querySelector(".details ol").innerHTML = "";
+    
+
+  });
+
+  try {
+
+    let res = await fetch(API + "lookup.php?i="+recipeId);
+    let data = await res.json();
+    let recipe = data.meals[0];
+
+    screen.recipe.querySelector(".thumbnail img").src = recipe.strMealThumb;
+    screen.recipe.querySelector(".details h2").innerText = recipe.strMeal;
+    
+
+    for (var i = 1; i <= 20; i++) {
+
+      if (recipe["strIngredient"+i].length == 0) {
+
+        break;
+
+      }
+
+      let li = document.createElement("li");
+      li.innerText = recipe["strIngredient"+i] + " - " + recipe["strMeasure"+i];
+      
+      screen.recipe.querySelector(".details ul").appendChild(li);
+
+    }
+
+    let instructions = recipe.strInstructions.split("\r\n").filter(v =>  v);
+    
+    for (let i = 0; i < instructions.length; i++) {
+      
+      let li = document.createElement("li");
+      li.innerText = instructions[i];
+      
+      screen.recipe.querySelector(".details ol").appendChild(li);
+      
+    }
+
+  } catch (msg) {}
+
+}
